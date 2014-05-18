7dbdc214ee52d0b8a93cbd98d66120e9cd1bf044
Christopher <chris.mike.barrett@gmail.com>
2014-05-18 15:58:48 -0700

    sorry forgot to commit this

---
+++ ClassProject/src/controller/MCP.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * @author Christopher
+ *
+ */
+public class MCP 
+{
+	/**
+	 * 
+	 */
+	public void MCP()
+	{
+		loadFiles();
+		loadGUI();
+	}
+	
+	private void loadFiles()
+	{
+		
+	}
+	
+	private void loadGUI()
+	{
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
