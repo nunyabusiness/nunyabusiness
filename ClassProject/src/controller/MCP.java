584fe10bd14342b5ac8d99bce2c7f44ecff5d58e
Christopher <chris.mike.barrett@gmail.com>
2014-05-23 14:38:55 -0700

    I am a long way from my ready with the controller

--- ClassProject/src/controller/MCP.java
+++ ClassProject/src/controller/MCP.java
@@ -3,23 +3,60 @@
  */
 package controller;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import model.Conference;
+import model.User;
+
 /**
  * @author Christopher
  *
  */
 public class MCP 
 {
+	Conference newCon;
+	
 	/**
+	 * @throws IOException 
 	 * 
 	 */
-	public void MCP()
+	public void MCP() throws IOException
 	{
+		newCon = new Conference(null);
 		loadFiles();
 		loadGUI();
 	}
 	
-	private void loadFiles()
+	private void loadFiles() throws FileNotFoundException
 	{
+		String UserFileName = "../files/usercsv.txt";
+		String PaperFileName = "../files/papercsv.txt";
+		BufferedReader fileIn;
+		
+		try 
+		{
+			fileIn = new BufferedReader(new FileReader(UserFileName));
+			
+			while (fileIn.ready())
+			{
+				String line = fileIn.readLine();
+				String[] portion = line.split(",");
+				
+				User person = new User(null);
+				person.setKey(0);
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.print(e);
+		}
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 		
 	}
 	
