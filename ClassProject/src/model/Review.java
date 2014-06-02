89551add5e46493c5f1de7b5f024198e5a4b7103
Christopher <chris.mike.barrett@gmail.com>
2014-06-02 11:00:43 -0700

    Updated Review do we want to do anything with Recomendation?

--- ClassProject/src/model/Review.java
+++ ClassProject/src/model/Review.java
@@ -1,5 +1,70 @@
 package model;
 
-public class Review {
+/**
+ * 
+ * @author Christopher Barrett
+ * I needed this for my part
+ */
+public class Review 
+{
+	private int score;
 	
+	private String comments;
+	
+	/**
+	 * Constructor
+	 */
+	public Review()
+	{
+		score = 0;
+		comments = "";
+	}
+	
+	/**
+	 * overloaded constructor
+	 * @param avg inputed average
+	 * @param comment inputed comment
+	 */
+	public Review(int avg, String comment)
+	{
+		score = avg;
+		comments = comment;
+	}
+	
+	/**
+	 * Setter
+	 * @param avg score from reviewer
+	 */
+	public void setScore(int avg)
+	{
+		score = avg;
+	}
+	
+	/**
+	 * Setter
+	 * @param comment comments from reviewer
+	 */
+	public void setComment(String comment)
+	{
+		comments = comment;
+	}
+	
+	/**
+	 * getter
+	 * @return average
+	 */
+	public int getScore()
+	{
+		return score;
+	}
+	
+	/**
+	 * getter
+	 * @return comments
+	 */
+	public String getComment()
+	{
+		return comments;
+	}
+	//how the f is this not been done yet?
 }
